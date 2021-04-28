34b86eadc9c6028086edf5d0af77ea1d386da31a
Baikang <alanbai84@gmail.com>
2021-04-28 21:50:44 +1000

    Create Bowling solution
    1. Add Bowling.Api project.
    2. Add Bowling.Test project.
    3. Add Swagger package.
    4. Add Newtonsoft.Json package.

---
+++ src/Bowling/Bowling.Api/Controllers/BowlingScoreController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Bowling.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BowlingScoreController : ControllerBase
+    {
+
+
+        private readonly ILogger<BowlingScoreController> _logger;
+
+        public BowlingScoreController(ILogger<BowlingScoreController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("Calculator")]
+        public IActionResult Calculator()
+        {
+            return Ok();
+        }
+    }
+}
